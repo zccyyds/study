f326aec3765eb855d5f109a55c757cf34034948b
闪光 <1847129346@qq.com>
2021-07-26 22:59:24 +0800

    删除了所有文件

--- study_snabbdom/src/demo.js
+++
@@ -1,9 +0,0 @@
-function aaa(){
-    console.log(2222222)
-}
-
-var aa = {c: aaa()}
-aa.c
-if(!(typeof aa.c == 'Object')){
-    console.log('111111111')
-}
--- study_snabbdom/src/mySnabbdom/vnode.js
+++
@@ -1,9 +0,0 @@
-/*
-* vnode方法是为了返回一个虚拟节点对象 
-*/
-
-export default function vnode(sel, data, children, text, elm) {
-    return {
-        sel, data, children, text, elm
-    }
-}
